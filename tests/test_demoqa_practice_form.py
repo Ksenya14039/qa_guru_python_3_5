2a2f3f1f346424562ee786b097414bc0761ec29b
ksenya14039 <ksenya14039@mail.ru>
2023-01-22 22:07:46 +0300

    add scroll

--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -6,24 +6,18 @@ picture_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'photo', 
 
 def test_filling_form():
     browser.open('/automation-practice-form')
-    (
-        browser.all('[id^=google_ads][id$=container__]').with_(timeout=10)
-        .should(have.size_greater_than_or_equal(2))
-        .perform(command.js.remove)
-    )
     browser.element('#firstName').type('Ksenia')
     browser.element('#lastName').type('Kapranova')
     browser.element('#userEmail').type('ksenya14039@mail.ru')
     browser.element('[for="gender-radio-2"]').click()
     browser.element('#userNumber').type(9973655228)
-
+    browser.element('#state').perform(command.js.scroll_into_view)
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('[value="9"]').click()
     browser.element('.react-datepicker__year-select').click()
     browser.element('[value="1997"]').click()
     browser.element('.react-datepicker__day--009').click()
-
     browser.element('#subjectsInput').should(be.blank).type('English').press_enter()
     browser.element('[for="hobbies-checkbox-2"]').click()
 
